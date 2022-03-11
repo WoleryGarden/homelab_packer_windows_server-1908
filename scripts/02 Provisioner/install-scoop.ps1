ff7ee7d4f960912f716d3708054b940e17bc1944
Andrew Savinykh <658865+AndrewSav@users.noreply.github.com>
2022-03-12 11:51:22 +1300

    update not to fail after latest scoop changes

--- scripts/02 Provisioner/install-scoop.ps1
+++ scripts/02 Provisioner/install-scoop.ps1
@@ -1,5 +1,6 @@
 Write-Host "Installing Scoop..."
-iwr -useb get.scoop.sh | iex
+iwr -useb get.scoop.sh -outfile 'install.ps1'
+.\install.ps1 -RunAsAdmin
 scoop install git
 scoop bucket add extras
 "Done installing scoop. Exit code: $LASTEXITCODE" | Write-Host
