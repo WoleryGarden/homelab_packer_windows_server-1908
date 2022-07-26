3f779890672762cb0812a8c073d0e9173af89ce4
Andrew Savinykh <658865+AndrewSav@users.noreply.github.com>
2022-07-27 11:53:41 +1200

    attempt to fix failed build

--- scripts/02 Provisioner/install-bginfo.ps1
+++ scripts/02 Provisioner/install-bginfo.ps1
@@ -6,7 +6,7 @@ Expand-Archive "C:\Program Files\BGInfo\bginfo.zip" -DestinationPath "C:\Program
 Copy-Item  "A:\BTServer.bgi" "C:\Program Files\BGInfo\BTServer.bgi"
 $RegistryKey = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
 $RegistryEntry = "RunBgInfo"
-$Command = '"C:\Program Files\BGInfo\bginfo.exe" "C:\Program Files\BGInfo\BTServer.bgi" /timer:0 /silent /nolicprompt'
+$Command = '"C:\Program Files\BGInfo\bginfo64.exe" "C:\Program Files\BGInfo\BTServer.bgi" /timer:0 /silent /nolicprompt'
 Set-ItemProperty -Path $RegistryKey -Name $RegistryEntry -Value $Command
 cmd /c $Command
 Write-Host "Done Installing BgInfo"
